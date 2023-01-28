f4f6eb539d79bc8f12f2b4313597775be9113093
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2023-01-28 17:11:15 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+// copy-paste your URL provided in your Alchemy.com dashboard
+const ALCHEMY_URL = "https://eth-mainnet.g.alchemy.com/v2/V9QniLcFpQjrpYVQITelw1cZY5Jspg60";
+
+async function transactionCount(){
+  const latest_block = await axios.post(ALCHEMY_URL, {
+    jsonrpc: "2.0",
+    id: 1,
+    method: "eth_getBlockTransactionCountByNumber",
+    params: ["finalized"]
+  
+  }).then((response) => {
+    console.log(response.data.result);
+  });
+}
+
+console.log(transactionCount())
+module.exports = transactionCount
