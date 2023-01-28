bf4ffef9f3975d358206d60173f132a741470be9
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2023-01-28 17:08:39 +0100

    Create batch_call.js

---
+++ batch_call.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+// copy-paste your URL provided in your Alchemy.com dashboard
+const ALCHEMY_URL = "https://eth-mainnet.g.alchemy.com/v2/V9QniLcFpQjrpYVQITelw1cZY5Jspg60";
+
+async function transactionCount(){
+    
+  const latest_block = {
+
+    jsonrpc: "2.0",
+    id: 1,
+    method: "eth_getBlockTransactionCountByNumber",
+    params: ["finalized"]
+  
+  };
+
+  const eth_balance = {
+
+    jsonrpc: "2.0",
+    id: 2,
+    method: "eth_getBalance",
+    params: ["0x07a812F11ff9857C7Db91bcD58b968AAaC0A86ED", "latest"]
+
+
+  }
+
+  const result = await axios.post(ALCHEMY_URL, ([latest_block, eth_balance])
+  ).then((response) => {
+    console.log(response.data);
+  })
+  
+}
+
+console.log(transactionCount())
+
